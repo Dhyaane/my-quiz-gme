e03b975253e858960a8f79ba8d1b66b5a93bb743
Dhyaane <73380856+Dhyaane@users.noreply.github.com>
2021-05-10 12:42:19 +0530

    Add files via upload

---
+++ Proc-37-template-main/sketch.js
@@ -0,0 +1,38 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
+
+if(allConstants !== undefined){
+fill("red");
+textSize("50");
+text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230);
+
+
+}
